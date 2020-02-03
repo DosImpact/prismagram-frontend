b6ffbcacded02f920d2879f84b7300788901d90a
DosImpact <ypd03008@gmail.com>
2020-02-03 22:52:48 +0900

    #6.0 copy Header UI

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import GlobalStyles from "../Styles/GlobalStyles";
 import styled, { ThemeProvider } from "styled-components";
-import Theme from "../Styles/Theme";
-import Router from "./Router";
+import { HashRouter } from "react-router-dom";
 import { gql } from "apollo-boost";
 import { useQuery } from "react-apollo-hooks";
-import Footer from "./Footer";
+
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+
+import Theme from "../Styles/Theme";
+import Router from "./Router";
+import Footer from "./Footer";
+import Header from "./Header";
 
 const QUERY = gql`
   {
@@ -16,7 +20,7 @@ const QUERY = gql`
 `;
 const Wrapper = styled.div`
   margin: 0 auto;
-  max-width: 935px;
+  max-width: ${props => props.theme.maxWidth};
   width: 100%;
 `;
 
@@ -27,12 +31,19 @@ export default () => {
 
   return (
     <ThemeProvider theme={Theme}>
-      <Wrapper>
+      <>
         <GlobalStyles />
-        <Router isLoggedIn={isLoggedIn} />
-        <Footer />
+        <HashRouter>
+          <Wrapper>
+            <>
+              <Header />
+              <Router isLoggedIn={isLoggedIn} />
+              <Footer />
+            </>
+          </Wrapper>
+        </HashRouter>
         <ToastContainer position={toast.POSITION.BOTTOM_LEFT} />
-      </Wrapper>
+      </>
     </ThemeProvider>
   );
 };
